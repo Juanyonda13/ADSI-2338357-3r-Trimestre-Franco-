a7fd515817e871d8b8d83548954c11da6e984a3d
Juanyonda13 <99970949+Juanyonda13@users.noreply.github.com>
2022-04-07 12:19:19 -0500

    Add files via upload

---
+++ Proyecto_SuperFinal/Index.php
@@ -0,0 +1,126 @@
+<?php
+    require_once("Templete/templateIndex.php");
+?>
+<!-- Menu del Index -->
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap" _mstaria-label="138918"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a class="navbar-brand" href="#"><img src="Imagen/qhatu.png" width="60" height="60" alt="Qhatu"></a></li>
+          <li><a href="#" class="nav-link px-2 text-white" _msthash="1633541" _msttexthash="136006">Qhatu</a></li>
+          
+        </ul>
+
+        <div class="text-end">
+          <a href="./Comprador/Vista/add.php"> <button type="button" class="btn btn-warning" _msthash="1718041" _msttexthash="176449"><i class="bi bi-person-plus-fill"> Registrar </i></button></a>  
+          <a href="./Comprador/Vista/login.php"><button type="button" class="btn btn-outline-light me-2" _msthash="1717859" _msttexthash="264433">Iniciar sesión</button></a>
+          <a href="./Comprador/Vista/index.php"><button type="button" class="btn btn-outline-light me-2" _msthash="1717859" _msttexthash="264433">Consulta</button></a>
+        </div>
+      </div>
+    </div>
+  </header>
+    <br><br>
+    <!-- Contenedores de elementos  imagen circulo texto -->
+    <center>
+    <div class="row">
+      <div class="col-lg-4">
+      <img src="Imagen/bufanda.png" width="100" height="100" alt="Qhatu">
+        <h2 _msthash="970905" _msttexthash="152542">Bufanda</h2>
+        <p _msthash="929318" _msttexthash="10116535">Bufanda para hombre tradicional indígena tribal estampada.</p>
+        <p><a class="btn btn-secondary" href="#" _msthash="1115764" _msttexthash="224497">Ver detalles »</a></p>
+      </div><!-- /.col-lg-4 -->
+      
+      <div class="col-lg-4 " >
+      <img src="Imagen/jarron.png" width="100" height="100" alt="Qhatu">  
+        <h2 _msthash="971113" _msttexthash="152542">Jarrón</h2>
+        <p _msthash="929513" _msttexthash="8021013">Jarron indigena elaborado en ceramica.</p>
+        <p><a class="btn btn-secondary" href="#" _msthash="1116011" _msttexthash="224497">Ver detalles »</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+      <img src="Imagen/mochila.png" width="100" height="100" alt="Qhatu">
+        <h2 _msthash="971321" _msttexthash="152542">Mochila</h2>
+        <p _msthash="929708" _msttexthash="4547907">Mochila indigena tejida a mano.</p>
+        <p><a class="btn btn-secondary" href="#" _msthash="1116258" _msttexthash="224497">Ver detalles »</a></p>
+      </div><!-- /.col-lg-4 -->
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+      <img src="Imagen/collar.png" width="100" height="100" alt="Qhatu">
+        <h2 _msthash="970905" _msttexthash="152542">Collar en mostacilla</h2>
+        <p _msthash="929318" _msttexthash="10116535">Collar en mostacilla elaborado artesanalmente.</p>
+        <p><a class="btn btn-secondary" href="#" _msthash="1115764" _msttexthash="224497">Ver detalles »</a></p>
+      </div><!-- /.col-lg-4 -->
+      
+      <div class="col-lg-4 " >
+      <img src="Imagen/flecha.png" width="100" height="100" alt="Qhatu">  
+        <h2 _msthash="971113" _msttexthash="152542">Flecha con veneno</h2>
+        <p _msthash="929513" _msttexthash="8021013">Flecha con veneno de rana especial para la caza de animales.</p>
+        <p><a class="btn btn-secondary" href="#" _msthash="1116011" _msttexthash="224497">Ver detalles »</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+      <img src="Imagen/arco.png" width="100" height="100" alt="Qhatu">
+        <h2 _msthash="971321" _msttexthash="152542">Arco</h2>
+        <p _msthash="929708" _msttexthash="4547907">Arco elaborado en madera .</p>
+        <p><a class="btn btn-secondary" href="#" _msthash="1116258" _msttexthash="224497">Ver detalles »</a></p>
+      </div><!-- /.col-lg-4 -->
+    </div>
+    
+    </center>
+    <br><br>
+  <!-- Contenedores de elementos  imagen cuadro texto -->
+  <div class="row">
+  <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary" _msthash="1515657" _msttexthash="60918">Mundo</strong>
+          <h3 class="mb-0" _msthash="1273116" _msttexthash="306839">Entrada destacada</h3>
+          <div class="mb-1 text-muted" _msthash="1336166" _msttexthash="48880">nov 12</div>
+          <p class="card-text mb-auto" _msthash="1224925" _msttexthash="7882225">Esta es una tarjeta más amplia con texto de apoyo a continuación como una introducción natural a contenido adicional.</p>
+          <a href="#" class="stretched-link" _msthash="1223677" _msttexthash="234481">Seguir leyendo</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+        
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Marcador de posición: Miniatura" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="809640"><title _mstHash="1337063" _mstTextHash="112281">Marcador</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em" _mstHash="1755052" _mstTextHash="135850">Miniatura</text></svg>
+          </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success" _msthash="1516073" _msttexthash="95472">Diseño</strong>
+          <h3 class="mb-0" _msthash="1273480" _msttexthash="546507">Título de la publicación</h3>
+          <div class="mb-1 text-muted" _msthash="1336543" _msttexthash="48737">nov 11</div>
+          <p class="mb-auto" _msthash="1225276" _msttexthash="7882225">Esta es una tarjeta más amplia con texto de apoyo a continuación como una introducción natural a contenido adicional.</p>
+          <a href="#" class="stretched-link" _msthash="1224028" _msttexthash="234481">Seguir leyendo</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Marcador de posición: Miniatura" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="809640"><title _mstHash="1337440" _mstTextHash="112281">Marcador</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em" _mstHash="1755520" _mstTextHash="135850">Miniatura</text></svg>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <br>
+</br>
+<br>
+</br>
+  <header class="p-3 bg-dark text-white">
+<div class="container">
+  <footer class="py-1 my-1">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2021 Company, Inc</p>
+  </footer>
+</div>
+</header>
